f37c848f7f06e6ee2cd06aafa0b61c532e664831
digi-21 <testmailhuyaar@gmail.com>
2024-09-12 21:08:37 +0530

    added export

--- Models/User.js
+++ Models/User.js
@@ -10,3 +10,5 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     phoneNumber: {type: String, required: true}
 })
+
+module.exports = mongoose.model('User', userSchema);
