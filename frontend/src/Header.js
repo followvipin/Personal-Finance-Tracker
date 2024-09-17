367ca480ecf63f064ed03e5f27b0c1fdbba83f1c
digi-21 <testmailhuyaar@gmail.com>
2024-09-17 22:09:20 +0530

    header added

---
+++ frontend/src/Header.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Header.css"; // CSS file for styling the header
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">Financlo</div>
+      <nav className={`nav ${isMenuOpen ? "nav--open" : ""}`}>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#home" className="nav__link">Team</a>
+          </li>
+          <li className="nav__item">
+            <a href="#about" className="nav__link">Donate</a>
+          </li>
+          <li className="nav__item">
+            <a href="#services" className="nav__link">Careers</a>
+          </li>
+          <li className="nav__item">
+            <a href="#contact" className="nav__link">About</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <span className="menu-toggle__bar"></span>
+        <span className="menu-toggle__bar"></span>
+        <span className="menu-toggle__bar"></span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
