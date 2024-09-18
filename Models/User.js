aa604cddb0dd31ecf0ad7783464d395b8a5348cd
Hitesh Agarwal <122112313+garghitesh1905@users.noreply.github.com>
2024-09-18 22:49:07 +0530

    Update User.js

--- Models/User.js
+++ Models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     full_name: { type: String, required: true },
     user_name: { type: String, required: true },
     account_balance: { type: Number, required: true},
-    cash_amount: { type: Date, default: Date.now },
+    cash_amount: { type: Number, required: true },
     expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'transactionSchema' }],
     email: { type: String, required: true },
     password: { type: String, required: true },
